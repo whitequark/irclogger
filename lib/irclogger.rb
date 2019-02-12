3e47319434c12a50b5fd92eb9dc4eaa0b4c8d8b9
whitequark <whitequark@whitequark.org>
2019-02-12 07:52:16 +0000

    Update dependencies.

--- lib/irclogger.rb
+++ lib/irclogger.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'sequel'
 
 Config = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'config', 'application.yml'))
-DB     = Sequel.connect(Config['database'], :encoding => Config['database'].start_with?('mysql') ? 'utf8mb4' : 'utf8')
+DB     = Sequel.connect(Config['database'], :encoding => Config['database'].start_with?('mysql') ? 'utf8mb4' : 'utf8').extension(:auto_literal_strings)
 
 require 'irclogger/message'
 require 'irclogger/channel'
