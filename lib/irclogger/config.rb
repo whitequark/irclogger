5e95155fb3dc481fbacd7b5ede45008fdc088739
Catherine <whitequark@whitequark.org>
2024-05-18 23:58:11 +0000

    Create log and tmp directories if they do not exist.

--- lib/irclogger/config.rb
+++ lib/irclogger/config.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'fileutils'
 
 root_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 Config = YAML.load_file(ENV["IRCLOGGER_CONFIG"] || File.join(root_dir, 'config', 'application.yml'))
@@ -6,3 +7,4 @@ Config['files'] = {
     'log' => File.expand_path(Config['files']['log'], root_dir),
     'tmp' => File.expand_path(Config['files']['tmp'], root_dir),
 }
+FileUtils.mkdir_p [Config['files']['log'], Config['files']['tmp']]
